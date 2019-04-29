cbc442b377a51e0300f3826e4c362917a20d9ec7
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2019-04-29 14:17:42 +0300

    Adding typescript to create-react-app

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    target: "web",
+    mode: "development",
+    entry: "./src/index.tsx",
+    output: {
+        path: null,
+        filename: "bundle.js"
+    },
+    module: null,
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
+    devServer: {
+        contentBase: "./",
+        port: 3000
+    }
+};
